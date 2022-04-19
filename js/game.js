a37af0066e27296dcecfb1d87fe7826dc815a482
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-04-19 21:57:52 +0000

    center and auto shrink

--- js/game.js
+++ js/game.js
@@ -1,10 +1,30 @@
 /* global Phaser */
 
+// Copyright (c) 2020 Mark Palfi All rights reserved
+//
+// Created by: Mark Palfi
+// Created on: Apr 2022
+// This is the Phaser3 configuration file
+
+//Game scene*/
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
-backgroundColor: 0x5f6e7a
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+    debug:true
+    }
+  },
+  //set background colour*/  
+  backgroundColor: 0x5f6e7a,
+  scale:{
+   mode: Phaser.Scale.FIT,
+  //we place it in the middle of the page.
+  autoCenter: Phaser.Scale.Center_BOTH,
+  }
 }
+
 const game = new Phaser.Game(config)
 console.log(game)
